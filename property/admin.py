797c5824f21b75017dd71ce359ec6e4ae782f176
cok263 <cok26385@gmail.com>
2020-11-25 00:54:42 +0300

    add editable and display field lists

--- property/admin.py
+++ property/admin.py
@@ -6,6 +6,10 @@ from .models import Flat
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town','address',)
     readonly_fields = ('created_at',)
+    list_display = ('address', 'price', 'new_building',
+                    'construction_year', 'town',)
+    list_editable = ('new_building',)
+    list_display_links = None
 
 
 admin.site.register(Flat, FlatAdmin)
